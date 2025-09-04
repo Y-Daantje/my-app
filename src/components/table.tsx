9db06e35935b29112c26b035d135544a5d844cf6
Y-Daantje <302867649@student.rocmondriaan.nl>
2025-09-04 16:50:53 +0200

    remove icon border

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,18 +1,34 @@
 // import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Paper from '@mui/material/Paper';
-import { MOCK_ACTIONS } from '../components/list';
-
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Paper from "@mui/material/Paper";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { MOCK_ACTIONS } from "../components/list";
+import { Box, IconButton, Typography } from "@mui/material";
+import React from "react";
 
 export default function ActionsList() {
+  const [likedItems, setLikedItems] = React.useState<{
+    [key: string]: boolean;
+  }>({});
+
+  const handleLikeClick = (id: string) => {
+    setLikedItems((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+
   return (
     <Paper
       sx={{
-        maxHeight: 440,
-        mt: 2,
+        maxHeight: 740,
+        display: "flex",
+        flexDirection: "column",
         overflow: "auto",
+        width: 350,
       }}
     >
       <List dense>
@@ -24,22 +40,58 @@ export default function ActionsList() {
               py: 1,
               "&:hover": {
                 backgroundColor: "action.hover",
+                cursor: "pointer",
               },
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "stretch",
+              backgroundColor: "#e4e2e2ff",
             }}
           >
-            <ListItemText
-              primary={action.name}
-              secondary={action.description}
-              slotProps={{
-                primary: {
-                  variant: "subtitle1",
-                  fontWeight: "medium",
-                },
-                secondary: {
-                  variant: "body2",
-                },
+            {/* Top row with name and heart */}
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                width: "100%",
+                mb: 1,
               }}
-            />
+            >
+              <Typography variant="subtitle1" sx={{ fontWeight: "medium" }}>
+                {action.name}
+              </Typography>
+              <IconButton
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleLikeClick(action.id);
+                }}
+                sx={{
+                  color: "#ff0000ff",
+                  "&:hover": { color: "red" },
+                  p: 0,
+                  "&:focus": { outline: "none" },
+                }}
+              >
+                {likedItems[action.id] ? (
+                  <FavoriteIcon sx={{ fontSize: 20, color: "red" }} />
+                ) : (
+                  <FavoriteBorderIcon sx={{ fontSize: 20 }} />
+                )}
+              </IconButton>
+            </Box>
+
+            {/* Description taking full width */}
+            <Typography
+              variant="body2"
+              sx={{
+                width: "100%",
+                wordBreak: "break-word",
+                overflowWrap: "break-word",
+              }}
+            >
+              {action.description}
+            </Typography>
           </ListItem>
         ))}
       </List>
